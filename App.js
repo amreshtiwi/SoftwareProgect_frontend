fede49265d681da71177644fe714d121608f28a6
amreshtiwi <ammr64@outlook.com>
2023-03-01 23:49:07 +0200

    create new project and start with login page

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import LoginPage from './pages/login';
+import Colors from './src/color';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <>
       <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        <LoginPage></LoginPage>
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: Colors.lightVanilla,
   },
 });
---
+++ src/color.js
@@ -0,0 +1,10 @@
+const Colors = {
+    darkGreen: "#1C6758",
+    lightGreen: "#3D8361",
+    yellow: "#DBBA42",
+    darkVanilla: "#D6CDA4",
+    lightVanilla: "#EEF2E6",
+    black: "#282525"
+};
+
+export default Colors;
